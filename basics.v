5821844eadab33a00c2ddad3dc170349ace80ca0
Ishaan Verma <ishaan56verma@gmail.com>
2021-10-02 19:07:33 +0530

    add basics

---
+++ basics.v
@@ -0,0 +1,346 @@
+Inductive bool : Type :=
+| true
+| false.
+
+Definition negb (b:bool) : bool :=
+  match b with
+  | true => false
+  | false => true
+  end.
+
+Definition andb (a:bool) (b:bool) : bool :=
+  match a with
+  | true => b
+  | false => false
+  end.
+
+Definition orb (a:bool) (b:bool) : bool :=
+  match a with
+  | true => true
+  | false => b
+  end.
+
+Example test_orb1: (orb true false) = true.
+Proof. simpl. reflexivity. Qed.
+
+Example test_orb2: (orb false false) = false.
+Proof. simpl. reflexivity. Qed.
+
+Example test_orb3: (orb false true) = true.
+Proof. simpl. reflexivity. Qed.
+
+Example test_orb4: (orb true true) = true.
+Proof. simpl. reflexivity. Qed.
+
+Definition nandb (b1:bool) (b2:bool) : bool :=
+  match b1 with
+  | true => negb b2
+  | false => negb b1
+  end.
+
+Example test_nandb1: (nandb true false) = true.
+Proof. simpl. reflexivity. Qed.
+
+Example test_nandb2: (nandb false false) = true.
+Proof. simpl. reflexivity. Qed.
+
+Example test_nandb3: (nandb false true) = true.
+Proof. simpl. reflexivity. Qed.
+
+Example test_nandb4: (nandb true true) = false.
+Proof. simpl. reflexivity. Qed.
+
+Definition andb3 (b1:bool) (b2:bool) (b3:bool) :=
+  match b1 with
+  | true => andb b2 b3
+  | false => false
+  end.
+
+Example test_andb31: (andb3 true true true) = true.
+Proof. simpl. reflexivity. Qed.
+
+Example test_andb32: (andb3 false true true) = false.
+Proof. simpl. reflexivity. Qed.
+
+Example test_andb33: (andb3 true false true) = false.
+Proof. simpl. reflexivity. Qed.
+
+Example test_andb34: (andb3 true true false) = false.
+Proof. simpl. reflexivity. Qed.
+
+Check negb.
+
+Notation "x && y" := (andb x y).
+Notation "x || y" := (orb x y).
+
+Inductive rgb : Type :=
+| red
+| green
+| blue.
+
+Inductive color : Type :=
+| black
+| white
+| primary (p : rgb).
+
+Definition monochrome (c:color) : bool :=
+  match c with
+  | black => true
+  | white => true
+  | primary p => false
+  end.
+
+
+Definition isred (c:color) : bool :=
+  match c with
+  | black => false
+  | white => false
+  | primary red => true
+  | primary _ => false
+  end.
+
+
+Example test_isred_1: (isred black) = false.
+Proof. simpl. reflexivity. Qed.
+
+Module Playground.
+  Definition b : rgb := blue.
+End Playground.
+
+Definition b : bool := true.
+
+Check Playground.b.
+Check b.
+
+
+Module TuplePlayground.
+  Inductive bit : Type :=
+  | B0
+  | B1.
+
+  Inductive nybble : Type :=
+  | bits (b0 b1 b2 b3 : bit).
+
+  Check (bits B1 B0 B1 B0).
+
+  Definition all_zero (nb : nybble) : bool :=
+    match nb with
+    | (bits B0 B0 B0 B0) => true
+    | (bits _ _ _ _) => false
+    end.
+
+  Compute (all_zero (bits B1 B0 B1 B0)).
+  Compute (all_zero (bits B0 B0 B0 B0)).
+End TuplePlayground.
+
+Module NatPlayground.
+  Inductive nat : Type :=
+  | O
+  | S (n : nat).
+
+  Fixpoint even (n : nat) : bool :=
+    match n with
+    | O => true
+    | S O => false
+    | S (S n') => even n'
+    end.
+
+  Definition odd (n : nat) : bool :=
+    negb (even n).
+End NatPlayground.
+
+Module NatPlayground2.
+  Fixpoint plus (n m : nat) : nat :=
+    match n with
+    | O => m
+    | S n' => S (plus n' m)
+    end.
+
+  Compute (plus 3 2).
+
+  Fixpoint mult (n m : nat) : nat :=
+    match n with
+    | O => O
+    | S n' => plus m (mult n' m)
+    end.
+
+  Example test_mult1: (mult 3 3) = 9.
+  Proof.  reflexivity. Qed.
+
+  Fixpoint minus (n m:nat) : nat :=
+    match n, m with
+    | O , _ => O
+    | S _ , O => n
+    | S n', S m' => minus n' m'
+    end.
+End NatPlayground2.
+
+Fixpoint exp (base power : nat) : nat :=
+  match power with
+  | O => S O
+  | S p => mult base (exp base p)
+  end.
+
+Fixpoint factorial (n : nat) : nat :=
+  match n with
+  | O => S O
+  | S n' => (S n') * (factorial n')
+  end.
+
+Example test_factorial1: (factorial 3) = 6.
+Proof. reflexivity. Qed.
+Example test_factorial2: (factorial 5) = 10 * 12.
+Proof. reflexivity. Qed.
+
+
+Fixpoint eqb (n m : nat) : bool :=
+  match n , m with
+  | O , O => true
+  | O , S m' => false
+  | S n' , O => false
+  | S n' , S m' => eqb n' m'
+  end.
+
+Example test_eqb1: (eqb 3 3) = true.
+Proof. reflexivity. Qed.
+Example test_eqb2: (eqb 3 4) = false.
+Proof. reflexivity. Qed.
+
+Fixpoint leb (n m : nat) : bool :=
+  match n with
+  | O => true
+  | S n' =>
+      match m with
+      | O => false
+      | S m' => leb n' m'
+      end
+  end.
+Example test_leb1: leb 2 2 = true.
+Proof. simpl. reflexivity. Qed.
+Example test_leb2: leb 2 4 = true.
+Proof. simpl. reflexivity. Qed.
+Example test_leb3: leb 4 2 = false.
+Proof. simpl. reflexivity. Qed.
+
+Notation "x =? y" := (eqb x y) (at level 70) : nat_scope.
+Notation "x <=? y" := (leb x y) (at level 70) : nat_scope.
+Example test_leb3': (4 <=? 2) = false.
+Proof. simpl. reflexivity. Qed.
+
+Definition ltb (n m : nat) : bool :=
+  match (n <=? m) with
+  | true => negb (n =? m)
+  | false => false
+  end.
+
+Example test_ltb1: (ltb 2 2) = false.
+Proof. reflexivity. Qed.
+Example test_ltb2: (ltb 2 4) = true.
+Proof. reflexivity. Qed.
+Example test_ltb3: (ltb 4 2) = false.
+Proof. reflexivity. Qed.
+
+Theorem plus_O_n : forall n : nat, 0 + n = n.
+Proof. intros n. reflexivity. Qed.
+
+Theorem plus_1_l : forall n : nat, 1 + n = S n.
+Proof.
+  intros n. reflexivity.
+Qed.
+
+Theorem mult_0_l : forall n : nat, 0 * n = 0.
+Proof. intros n. reflexivity. Qed.
+
+Theorem plus_id_exercise : forall n m o : nat,
+    n = m -> m = o -> n + m = m + o.
+Proof.
+  intros n m o.
+  intros H.
+  rewrite -> H.
+  intros G.
+  rewrite -> G.
+  reflexivity.
+Qed.
+
+Theorem mult_n_0_m_0 : forall n m : nat,
+    (n * 0) + (m * 0) = 0.
+Proof.
+  intros n m.
+  rewrite <- mult_n_O.
+  rewrite <- mult_n_O.
+  reflexivity.
+Qed.
+
+Theorem mult_n_1 : forall n : nat,
+    n * 1 = n.
+Proof.
+  intros n.
+  rewrite <- mult_n_Sm.
+  rewrite <- mult_n_O.
+  reflexivity.
+Qed.
+
+Theorem plus_1_neq_0 : forall n : nat,
+    (n + 1) =? 0 = false.
+Proof.
+  intros n.
+  destruct n as [| n'] eqn:E.
+  - reflexivity.
+  - reflexivity.
+Qed.
+
+Theorem negb_involutive : forall b : bool,
+    negb (negb b) = b.
+Proof.
+  intros b.
+  destruct b eqn:E.
+  - reflexivity.
+  - reflexivity.
+Qed.
+
+Theorem andb_commutative : forall b c,
+    andb b c = andb c b.
+Proof.
+  intros b c.
+  destruct b eqn:Eb.
+  - destruct c eqn:Ec.
+    + reflexivity.
+    + reflexivity.
+  - destruct c eqn:Ec.
+    + reflexivity.
+    + reflexivity.
+Qed.
+
+
+Theorem andb3_exchange :
+  forall b c d, andb (andb b c) d = andb (andb b d) c.
+Proof.
+  intros b c d. destruct b eqn:Eb.
+  - destruct c eqn:Ec.
+    { destruct d eqn:Ed.
+      - reflexivity.
+      - reflexivity. }
+    { destruct d eqn:Ed.
+      - reflexivity.
+      - reflexivity. }
+  - destruct c eqn:Ec.
+    { destruct d eqn:Ed.
+      - reflexivity.
+      - reflexivity. }
+    { destruct d eqn:Ed.
+      - reflexivity.
+      - reflexivity. }
+Qed.
+
+Theorem andb_true_elim2 : forall b c : bool,
+    andb b c = true -> c = true.
+Proof.
+  intros b c.
+  destruct b eqn:Eb.
+  - { destruct c eqn:Ec.
+      - reflexivity.
+      - { intros H. rewrite <- H. reflexivity. } }
+  - { destruct c eqn:Ec.
+      - intros H. reflexivity.
+      - intros H. rewrite <- H. reflexivity. }
+Qed.
