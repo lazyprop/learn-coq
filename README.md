b0e69973d5cedf641e9265bbbfb0bb3d2b01c8a7
Ishaan Verma <ishaan56verma@gmail.com>
2021-10-03 23:55:50 +0530

    readme

---
+++ README.md
@@ -0,0 +1,10 @@
+Coq is very cool. Most of this will be solutions to Software Foundations
+exercises. Some of it will be stuff I implement for practice.
+
+resources
+- [Software Foundations series by Pierce et
+  al.](https://softwarefoundations.cis.upenn.edu/)
+
+use emacs + [proofgeneral](https://proofgeneral.github.io/). vim +
+[coqtail](https://github.com/whonore/Coqtail) is another option but is laggy and
+weird.
--- basics.v
+++ basics.v
@@ -336,11 +336,88 @@ Theorem andb_true_elim2 : forall b c : bool,
     andb b c = true -> c = true.
 Proof.
   intros b c.
-  destruct b eqn:Eb.
-  - { destruct c eqn:Ec.
+  destruct c eqn:Ec.
+  - { intros H.
+      reflexivity. }
+  - { intros H.
+      rewrite <- H.
+      destruct b eqn:Eb.
       - reflexivity.
-      - { intros H. rewrite <- H. reflexivity. } }
-  - { destruct c eqn:Ec.
-      - intros H. reflexivity.
-      - intros H. rewrite <- H. reflexivity. }
+      - reflexivity. }
 Qed.
+
+Theorem identity_fn_applied_twice :
+  forall (f : bool -> bool),
+    (forall (x : bool), f x = x) ->
+    forall (b : bool), f (f b) = b.
+Proof.
+  intros f H x.
+  destruct x eqn:Ex.
+  - { rewrite -> H.
+      rewrite -> H.
+      reflexivity. }
+  - { rewrite -> H.
+      rewrite -> H.
+      reflexivity. }
+Qed.
+
+Theorem negation_fn_applied_twice :
+  forall (f : bool -> bool),
+    (forall (x : bool), f x = negb x) ->
+    forall (b : bool), f (f b) = b.
+Proof.
+  intros f H x.
+  destruct x eqn:Ex.
+  - { rewrite -> H.
+      rewrite -> H.
+      reflexivity. }
+  - { rewrite -> H.
+      rewrite -> H.
+      reflexivity. }
+Qed.
+
+Theorem andb_eq_orb :
+  forall (b c : bool),
+    (andb b c = orb b c) ->
+    b = c.
+Proof.
+  intros b c.
+  destruct b eqn:Eb.
+  - simpl. intros H. rewrite -> H. reflexivity.
+  - simpl. intros H. rewrite -> H. reflexivity.
+Qed.
+
+Inductive bin : Type :=
+| Z
+| B0 (n : bin)
+| B1 (n : bin).
+
+
+Fixpoint incr (m : bin) : bin :=
+  match m with
+  | Z => B1 Z
+  | B0 m' => B1 m'
+  | B1 m' => B0 (incr m')
+  end.
+
+Example test_bin_incr1 : (incr (B1 Z)) = B0 (B1 Z).
+Proof. reflexivity. Qed.
+Example test_bin_incr2 : (incr (B0 (B1 Z))) = B1 (B1 Z).
+Proof. reflexivity. Qed.
+Example test_bin_incr3 : (incr (B1 (B1 Z))) = B0 (B0 (B1 Z)).
+Proof. reflexivity. Qed.
+
+Fixpoint bin_to_nat (m : bin) : nat :=
+  match m with
+  | Z => 0
+  | B0 m' => 2 * bin_to_nat m'
+  | B1 m' => 1 + 2 * bin_to_nat m'
+  end.
+
+Example test_bin_incr4 : bin_to_nat (B0 (B1 Z)) = 2.
+Proof. reflexivity. Qed.
+Example test_bin_incr5 : bin_to_nat (incr (B1 Z)) = 1 + bin_to_nat (B1 Z).
+Proof. reflexivity. Qed.
+Example test_bin_incr6 :
+        bin_to_nat (incr (incr (B1 Z))) = 2 + bin_to_nat (B1 Z).
+Proof. reflexivity. Qed.
